9c8e9d280a6772b8e13bae7ee8359f6472a090f0
“Mia <mb.mia.bosse@gmail.com>
2023-03-10 13:52:15 +0100

    cv page initial strukture

--- src/main.js
+++ src/main.js
@@ -1,28 +1,21 @@
 import "./style.css";
 
 // burgermenu
-// Lav variablen "btn", der henviser til ".toggle-btn"
 const btn = document.querySelector(".block button");
-// Lav variablen "menu", der henviser til ".main-menu"
 const menu = document.querySelector(".main-menu");
 const menuimg = btn.querySelector("img");
 
-// Lav funktionen "toggleMenu()" med følgende funktionalitet
 function toggleMenu() {
-  // a) toggle klassen "shown" på menu vha. classList.toggle
   menu.classList.toggle("shown");
-  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
   const menuShown = menu.classList.contains("shown");
-  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+
   if (menuShown) {
     console.log(menuShown); // se i konsollen
-    menuimg.src = "../svg/luk.svg"; // sæt btn.textContent til "Luk", hvis menuShown er "true"
+    menuimg.src = "../svg/luk.svg";
   } else {
-    console.log(menuShown); // se i konsollen
-    menuimg.src = "../svg/burgermenu.svg"; // sæt btn.textContent til "Menu", hvis menuShown er "false"
+    console.log(menuShown);
+    menuimg.src = "../svg/burgermenu.svg";
   }
-  // "toggleMenu()" slutter her
 }
 
-// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
 btn.addEventListener("click", toggleMenu);
