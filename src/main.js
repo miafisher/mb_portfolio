b4c2557b1e3568a5e37d39981db156fe99abe7d0
“Mia <mb.mia.bosse@gmail.com>
2023-03-28 09:37:07 +0200

    design header

--- src/main.js
+++ src/main.js
@@ -2,22 +2,22 @@ import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
 // burgermenu
-const btn = document.querySelector(".block button");
-const menu = document.querySelector(".main-menu");
-const menuimg = btn.querySelector("img");
+// const btn = document.querySelector(".block button");
+// const menu = document.querySelector(".main-menu");
+// const menuimg = btn.querySelector("img");
 
-function toggleMenu() {
-  menu.classList.toggle("shown");
-  const menuShown = menu.classList.contains("shown");
+// function toggleMenu() {
+//   menu.classList.toggle("shown");
+//   const menuShown = menu.classList.contains("shown");
 
-  if (menuShown) {
-    console.log(menuShown); // se i konsollen
-    menuimg.src = "../svg/luk.svg";
-  } else {
-    console.log(menuShown);
-    menuimg.src = "../svg/burgermenu.svg";
-  }
-}
+//   if (menuShown) {
+//     console.log(menuShown); // se i konsollen
+//     menuimg.src = "../svg/luk.svg";
+//   } else {
+//     console.log(menuShown);
+//     menuimg.src = "../svg/burgermenu.svg";
+//   }
+// }
 
 btn.addEventListener("click", toggleMenu);
 
